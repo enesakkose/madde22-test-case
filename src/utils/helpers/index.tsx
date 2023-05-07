7fe65ea23f65e3b5237109d38843548491ce6f4e
enesakkose <ens.akkose@gmail.com>
2023-05-07 18:51:19 +0300

    fix

--- src/utils/helpers/index.tsx
+++ src/utils/helpers/index.tsx
@@ -1,18 +1,6 @@
 import dayjs from 'dayjs'
-import useGetWindowSize from '@/utils/hooks/useGetWindowSize'
 import 'dayjs/locale/tr'
 
 export const getFormattedDate = (timestamp: number) => {
   return dayjs.unix(timestamp).locale('tr').format('D MMM ddd HH:mm')
-}
-
-
-export const getBreakPoint = (breakPoint: 'SM' | 'MD') => {
-  const size = useGetWindowSize()
-
-  if (breakPoint === 'SM') {
-    const SM = size === 'SM' || size === 'MD'
-
-    return SM
-  }
 }
